06f2fa39f433c2cf074ad2e8451f4666db13fb6c
Fredson Junior <fredson.cocco.jr@hotmail.com>
2021-03-30 16:45:54 -0300

    Commit 9

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -19,8 +19,11 @@ app.use(express.static("public"));
 let posts = [];
 
 app.get("/", function(req, res){
-    res.render("home", {StartingContent: homeStartingContent});
-    console.log(posts);
+    res.render("home", {
+      StartingContent: homeStartingContent,
+      posts: posts
+     });
+    
 });
 
 app.get("/about", function(req, res){
